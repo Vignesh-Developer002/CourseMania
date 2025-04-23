586821e4dd829a45a6cbf56e292a7926ce67249c
Vignesh <vigneshcoder002@gmail.com>
2025-04-23 19:02:15 +0530

    Update Course.jsx

--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -10,7 +10,7 @@ import { globalStore } from "./context/StoreContext.jsx";
 const Course = () => {
   // "http://localhost:4000/courses"
   const { courseData, error, isLoading } = fetchUserData(
-    "https://coursemania-backend.onrender.com/courses"
+    "http://192.168.1.82:4000/courses"
   );
   const { courseFilterData } = useContext(globalStore);
 
