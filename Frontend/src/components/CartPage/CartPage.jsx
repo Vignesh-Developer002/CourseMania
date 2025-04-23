0dc6b81d7d5d07c1caadaf4f2117f696e15cbdbe
Vignesh <vigneshcoder002@gmail.com>
2025-04-23 18:59:10 +0530

    Update CartPage.jsx

--- Frontend/src/components/CartPage/CartPage.jsx
+++ Frontend/src/components/CartPage/CartPage.jsx
@@ -19,9 +19,9 @@ import { fetchUserData } from "../data/data.jsx";
 const CartPage = () => {
   //  let courseUrl = "http://localhost:4000/courses"
   // let certificateUrl = "http://localhost:4000/Certification"
-  const { courseData } = fetchUserData("https://coursemania-backend.onrender.com/courses");
+  const { courseData } = fetchUserData("http://192.168.1.82:4000/courses");
   const { certificateData } = fetchUserData(
-    "https://coursemania-backend.onrender.com/Certification"
+    "http://192.168.1.82:4000/Certification"
   );
 
   const {
